9036d6b0cfceb9d59c05b33e026de2ea9cdf5a6c
Prashant Singh <mprashantks@gmail.com>
2018-02-25 02:04:57 +0530

    Made minor modifications

--- find-function/impl.cpp
+++ find-function/impl.cpp
@@ -4,8 +4,34 @@
 
 using namespace std;
 
-// First approach : Using custom code
+// First approach : Using Regular expression for better flexiblity and performance
+// Requires C++11 or higher compiler support
 unsigned int FindFunctionDefn(const char* strFunctionName, const char* strSourceCode) {
+  string funName(strFunctionName);
+  funName = "("+funName+"\\s*\\([\\w, ]*\\)\\s*\\{)";                           // Regular expression
+  regex r(funName);
+
+  int line = 1, i = 0, sourceCodeLen = strlen(strSourceCode);
+
+  while (i < sourceCodeLen) {
+    char tmp[sourceCodeLen];                                                    // Store each line
+    int k = 0;
+    while (!(strSourceCode[i]=='\\' && strSourceCode[i+1]=='n') && i<sourceCodeLen) {
+      tmp[k++] = strSourceCode[i];
+      i++;
+    }
+    if (regex_search(tmp,r))                                                    // Check using regex
+      return line;
+    while (strSourceCode[i]=='\\' && strSourceCode[i+1]=='n' && i<sourceCodeLen) {
+      i+=2;
+      line++;
+    }
+  }
+  return 0;
+}
+
+// Second approach : custom code
+unsigned int FindFunctionDefnCustom(const char* strFunctionName, const char* strSourceCode) {
   unsigned int line = 1;
   int i = 0, j = 0, sourceCodeLen = strlen(strSourceCode), functionNameLen = strlen(strFunctionName);
 
@@ -41,34 +67,11 @@ unsigned int FindFunctionDefn(const char* strFunctionName, const char* strSource
 }
 
 
-// Second approach : Using Regular expression for better flexiblity and performance in case of large source code
-unsigned int FindFunctionDefnReg(const char* strFunctionName, const char* strSourceCode) {
-  string funName(strFunctionName);
-  funName = "("+funName+"\\s*\\([\\w, ]*\\)\\s*\\{)";                           // Regular expression
-  regex r(funName);
-
-  int line = 1, i = 0, sourceCodeLen = strlen(strSourceCode);
-
-  while (i < sourceCodeLen) {
-    char tmp[sourceCodeLen];                                                    // Store each line
-    int k = 0;
-    while (!(strSourceCode[i]=='\\' && strSourceCode[i+1]=='n') && i<sourceCodeLen) {
-      tmp[k++] = strSourceCode[i];
-      i++;
-    }
-    if (regex_search(tmp,r))                                                    // Check using regex
-      return line;
-    while (strSourceCode[i]=='\\' && strSourceCode[i+1]=='n' && i<sourceCodeLen) {
-      i+=2;
-      line++;
-    }
-  }
-  return 0;
-}
-
 int main() {
   char strFunctionName[] = "func2";
   char strSourceCode[] = "int func1(){ return 0; }\\n int func2(){ return 1; }\\n" "int main(int argc, char*argv[]){ return func2(); }\\n";
-  cout << FindFunctionDefn( strFunctionName, strSourceCode ) << endl;
+  char strSourceCode2[] = "int func2 (int a, int b) { return 0; }\\n int func1 (int a) { return 1; }\\n" "int main(int argc, char*argv[]){ return func2(); }\\n";
+  cout << "Regex approach: " << FindFunctionDefn( strFunctionName, strSourceCode ) << endl;
+  cout << "Custom approach: " << FindFunctionDefnCustom( strFunctionName, strSourceCode ) << endl;
   return 0;
 }
